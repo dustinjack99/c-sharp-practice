74184e25715487776169764df55f7e1dcc6759fb
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-09-13 12:24:29 -0500

    finished array / method lesson

---
+++ ConsoleApp1/ConsoleApp1/Class3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class ClassArrayMethod
+    {
+        public void ArrayMethodLesson()
+        {
+            // Declaring / Updating Arrays
+            //int[] luckyNumbers = { 4, 5, 14, 52, 33, 3 };
+            //string[] friends = new string[5];
+
+            //friends[0] = "Ben";
+            //friends[1] = "Patrick";
+            //friends[2] = "Amber";
+            //friends[3] = "Tim";
+            //friends[4] = "Peter";
+
+            //luckyNumbers[2] = 41;
+
+            //Console.WriteLine(luckyNumbers[3]);
+
+            // Methods
+            //string name = Console.ReadLine();
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //SayHi(name, age);
+            int cubedNum = Cube(3);
+            Console.WriteLine(cubedNum);
+        }
+
+        private void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("You are:  " + age);
+        }
+
+        private int Cube(int num)
+        {
+            return num * num * num;
+            
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,20 +6,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Import Calculator App
-            //var Calc = new ClassCalculator();
-            //Calc.Calculator();
-
-            //Import Madlib App
-            var Mad = new ClassMadlib();
-            Mad.Madlib();
-
             //Variable / Datatype Lesson
             //Console.WriteLine("Enter your name, or else!");
             //string name = Console.ReadLine();
             //Console.WriteLine("Enter your age, or else!");
             //string age = Console.ReadLine();
             //Console.WriteLine("How's if goin', " + name + "? You are: " + age);
+
+            //Import Calculator
+            //var Calc = new ClassCalculator();
+            //Calc.Calculator();
+
+            //Import Madlib
+            //var Mad = new ClassMadlib();
+            //Mad.Madlib();
+
+            //Import Array / Method Lesson
+            var ArrMeth = new ClassArrayMethod();
+            ArrMeth.ArrayMethodLesson();
+
         }
     }
 }
