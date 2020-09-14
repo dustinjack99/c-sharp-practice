6026f52a1a376474ed8ad99a06ca2303f3083347
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-09-14 11:45:32 -0500

    inited files for MVC tutorial

---
+++ RealMvc/RealMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RealMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
