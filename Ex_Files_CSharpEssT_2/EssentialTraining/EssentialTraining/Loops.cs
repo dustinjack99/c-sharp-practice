68ef6bc50aafbd71bfc9891fd4ba5e6ec71b0818
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-09-29 23:10:13 -0700

    added loop lesson, which went over while and do/while with continue and break keywords

---
+++ Ex_Files_CSharpEssT_2/EssentialTraining/EssentialTraining/Loops.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+    class Loops
+    {
+        public string BreakAndContinue()
+        {
+            var sb = new StringBuilder();
+
+            var words = new List<string> { "Bread", "Milk", "Eggs", "Cheese", "Apples" };
+
+            foreach (var word in words)
+            {
+                //continue skips single iteration of loop
+                if (word.StartsWith("M")) continue;
+
+
+                //break ends the loop entirely
+                if (word.StartsWith("C")) break;
+
+                sb.AppendLine(word);
+            }
+
+            return sb.ToString();
+        }
+        public int DoWhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+
+            //condition evaluated after do block runs
+            //If you need block to run at least once, use do/while
+            do
+            {
+                sum += counter;
+                counter++;
+            } while (counter < 100);
+
+            return sum;
+        }
+        public int WhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+
+            //evaluates condition first
+            while(counter < 100)
+            {
+                sum += counter;
+                counter++;
+            }
+
+            return sum;
+        }
+    }
+}
